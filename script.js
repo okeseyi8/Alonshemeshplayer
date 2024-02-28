9e9993249612780886cc94efe2c52a9535df8147
OKe <Oke Oluwaseyi>
2024-02-28 10:42:43 +0100

    added new play buttone

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const playButton = document.getElementById("play");
+const playButtons = document.querySelectorAll(".main_play");
+let play_icons = document.querySelectorAll(".play-icon");
 const main_speaker = document.getElementById("main_speaker")
 const speaker_icon = document.getElementById("speaker-icon");
 const like_button = document.getElementById("like_button");
 const audio = document.getElementById("audio");
 const like_icon = document.getElementById("like_icon");
-let play_icon = document.getElementById("play-icon");
 let isPlaying = false;
 let isMute = false;
 let isLiked = false;
@@ -48,34 +48,40 @@ speaker_icon.addEventListener("click", () =>{
   
 
 })
- window.onload = function(){
-  seekBar.value = 100;
-  function playFunc(){
-  playButton.addEventListener("click", () => {
-    isPlaying = !isPlaying;
-    if (isPlaying) {
-        
-        play_icon.classList.replace("fa-play-circle", "fa-circle-pause");
-        
-        audio.play();
-    } else {
-        if (audio) {
+function playFunc(){
+  playButtons.forEach(function(button) {
+    button.addEventListener("click", () => {
+      isPlaying = !isPlaying;
+      
+      play_icons.forEach(function(play_icon){
+        if (isPlaying) {
+          play_icon.classList.replace("fa-play-circle", "fa-circle-pause");
+          audio.play();
+        } else {
+          if (audio) {
             audio.pause(); // Pause the audio
+          }
+          play_icon.classList.replace("fa-circle-pause", "fa-play-circle");
         }
-        play_icon.classList.replace("fa-circle-pause", "fa-play-circle");
-    }
-});
+      });
+    });
+  });
 }
-playFunc();
-like_button.addEventListener("click", () =>{
-  isLiked = !isLiked;
-  if(isLiked){
-    like_icon.classList.replace("fa-regular", "fa-solid")
-    document.cookie = "likedStation=Israel fm; expires=Thu, 01 Jan 2025 00:00:00 UTC; path=/";       
-  }else{
-    like_icon.classList.replace("fa-solid", "fa-regular")
-    document.cookie = "likedStation=; expires=Thu, 01 Jan 2025 00:00:00 UTC; path=/";
-  }
 
-})
+window.onload = function(){
+  seekBar.value = 100;
+  playFunc();
+  like_button.addEventListener("click", () => {
+    isLiked = !isLiked;
+    if(isLiked){
+      like_icon.classList.replace("fa-regular", "fa-solid")
+      document.cookie = "likedStation=Israel fm; expires=Thu, 01 Jan 2025 00:00:00 UTC; path=/";       
+    } else {
+      like_icon.classList.replace("fa-solid", "fa-regular")
+      document.cookie = "likedStation=; expires=Thu, 01 Jan 2025 00:00:00 UTC; path=/";
+    }
+  });
+  
 }
+
+
